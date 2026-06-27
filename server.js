d69a470a13e196adcbdf90062191c237d53513c4
Syed-Raheem <syedabdulraheem801@gmail.com>
2026-06-27 09:06:58 +0530

    Add email timeout config

--- server.js
+++ server.js
@@ -26,11 +26,16 @@ app.post("/api/contact", async (req, res) => {
     }
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+        },
+        connectionTimeout: 10000,
+        greetingTimeout: 10000,
+        socketTimeout: 10000,
     });
 
     await transporter.sendMail({
