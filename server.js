512bc728b3cda5bb1f56593b4907b82c970f38ef
Syed-Raheem <syedabdulraheem801@gmail.com>
2026-07-15 09:19:07 +0530

    forntend url updated2.0

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173" || "https://syedabdulraheem-unity.vercel.app",
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
 }));
 
 app.use(express.json());
